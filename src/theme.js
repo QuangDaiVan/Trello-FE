082868fed192ee9c4794e48ee6f19b32983f37c3
caloc <quangdv2802@gmail.com>
2024-08-12 15:29:59 +0700

    lession 25

---
+++ src/theme.js
@@ -0,0 +1,55 @@
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+// Create a theme instance.
+const theme = extendTheme({
+  trello: {
+    appBarHeight: '58px',
+    boardBarHeight: '60px'
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#dcdde1',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: 'white'
+          }
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          borderWidth: '0.5px',
+          '&:hover': { borderWidth: '0.5px' }
+        }
+      }
+    },
+
+    MuiInputLabel: {
+      styleOverrides: {
+        root: { fontSize: '0.875rem' }
+      }
+    },
+
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          fontSize: '0.875rem',
+          '& fieldset': { borderWidth: '0.5px !important' },
+          '&:hover fieldset': { borderWidth: '0.5px !important' },
+          '&.Mui-focused fieldset': { borderWidth: '1px !important' }
+        }
+      }
+    }
+  }
+})
+
+export default theme
